7b3f55c024bb5621767776165bfd56eedc4593d4
Zombatar33 <ageeadebied@gmail.com>
2023-01-25 17:26:25 +0100

    Made Audio-FX more performant and fixed routing

--- src/components/AudioManipulation.jsx
+++ src/components/AudioManipulation.jsx
@@ -6,28 +6,29 @@ function AudioManipulator({ videoRef }) {
     const audioSrcContainer = useRef(null);
     const audioScriptContainer = useRef(null)
 
-    var [_bitSamples, setBitSamples] = useState(7);
-    const [_bitcrushNormFrequency, setBitcrushNormFrequency] = useState(0.2);
+    const effectBitcrushContainer = useRef(null);
+    const effectDistortionContainer = useRef(null);
+    const effectBassBoostContainer = useRef(null);
+    const effectGainContainer = useRef(null);
+
+    const [_bitSamples, setBitSamples] = useState(7);
+    const [_normFrequency, setNormFrequency] = useState(0.1);
     const [_distortion, setDistortion] = useState(0);
     const [_bassBoost, setBassBoost] = useState(0);
-    const [_gain, setGain] = useState(1);
+    const [_gain, setGain] = useState(5);
 
     function handleResetClick() {
         setBitSamples(0);
-        setBitcrushNormFrequency(1);
         setDistortion(0);
         setBassBoost(0);
         setGain(1);
     }
 
     function handlePlay() {
-        if (audioCtxContainer.current.state === "running") {
-            audioCtxContainer.current.suspend();
-        }else {
-            audioCtxContainer.current.resume();
-        }
+        audioCtxContainer.current.resume();
     }
 
+    // Ready Video
     useEffect(() => {
         const video = videoRef.current;
         video.onplay = handlePlay;
@@ -36,6 +37,7 @@ function AudioManipulator({ videoRef }) {
         }
       }, []);
 
+      // Create Effects and Audio-API
     useEffect(() => {
         if (!audioCtxContainer.current) {
             audioCtxContainer.current = new AudioContext();
@@ -43,27 +45,42 @@ function AudioManipulator({ videoRef }) {
         if (!audioSrcContainer.current) {
             audioSrcContainer.current = audioCtxContainer.current.createMediaElementSource(videoRef.current);
         }
-
         if (!audioScriptContainer.current) {
             var bufferSize = 4096;
             audioScriptContainer.current = audioCtxContainer.current.createScriptProcessor(bufferSize, 1, 1);
         }
 
+        if (!effectBitcrushContainer.current) {
+            effectBitcrushContainer.current = createBitCrush(_bitSamples, _normFrequency)();
+        }
+        if (!effectDistortionContainer.current) {
+            effectDistortionContainer.current = audioCtxContainer.current.createWaveShaper();
+        }
+        if (!effectBassBoostContainer.current) {
+            effectBassBoostContainer.current = audioCtxContainer.current.createBiquadFilter();
+            effectBassBoostContainer.current.type = "lowshelf";
+            effectBassBoostContainer.current.frequency.value = 200;
+        }
+        if (!effectGainContainer.current) {
+            effectGainContainer.current = audioCtxContainer.current.createGain();
+        }
+    }, []);
 
-        //bitcrush
-        var bitcrush = (function () {
+    function createBitCrush(paramSamples, paramNormFrequency) {
+        return (function () {
             var bufferSize = 4096;
             var node = audioScriptContainer.current;
-            node.bits = _bitSamples; // between 1 and 16
-            node.normfreq = _bitcrushNormFrequency; // between 0.0 and 1.0
-            var step = Math.pow(1 / 2, node.bits);
+            var bits = 16 - paramSamples; // between 0 and 15
+            var normfreq = paramNormFrequency; // between 0.0 and 1.0
+            console.log(paramNormFrequency + " - " + normfreq);
+            var step = Math.pow(1 / 2, bits);
             var phaser = 0;
             var last = 0;
             node.onaudioprocess = function (e) {
                 var input = e.inputBuffer.getChannelData(0);
                 var output = e.outputBuffer.getChannelData(0);
                 for (var i = 0; i < bufferSize; i++) {
-                    phaser += node.normfreq;
+                    phaser += normfreq;
                     if (phaser >= 1.0) {
                         phaser -= 1.0;
                         last = step * Math.floor(input[i] / step + 0.5);
@@ -72,32 +89,59 @@ function AudioManipulator({ videoRef }) {
                 }
             };
             return node;
-        })();
+        })
+    }
 
-        // create distortion
-        var dist = audioCtxContainer.current.createWaveShaper();
-        // 0 bis 100 funktioniert sehr gut, ist aber egal
-        dist.curve = makeDistortionCurve(_distortion);
+    // Connect FX
+    useEffect(() => {
+        // First effect connected is last in chain
+        audioSrcContainer.current.connect(effectGainContainer.current);
+        effectGainContainer.current.connect(effectDistortionContainer.current);
+        effectDistortionContainer.current.connect(effectBitcrushContainer.current);
+        effectBitcrushContainer.current.connect(effectBassBoostContainer.current);
+        effectBassBoostContainer.current.connect(audioCtxContainer.current.destination);
+    }, []);
 
-        // create filter
-        var filter = audioCtxContainer.current.createBiquadFilter();
-        filter.type = "lowshelf";
-        filter.frequency.value = 200;
-        filter.gain.value = _bassBoost;
+    // Update FX-Parameters
+    useEffect(() => {
+        // soll nur ein mal ausgeführt werden
+        // nur ein mal effekte erstellen und connecten (nicht disconnecten)
 
-        // gain
-        var gain = audioCtxContainer.current.createGain();
-        gain.gain.value = _gain;
+        // firefox -> webaudioeditor
+        // chrome -> audion
+        // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Tools
 
-        // chain effects - first effect in chain is actually last
-        audioSrcContainer.current.connect(gain);
-        gain.connect(dist);
-        dist.connect(bitcrush);
-        bitcrush.connect(filter);
-        filter.connect(audioCtxContainer.current.destination);
+        // use effect kann weg
+
+
+        //bitcrush
+        // TODO: CHANGE BITCRUSH SAMPLES
+        var constrainedBitSamples = _bitSamples;
+        if (constrainedBitSamples < 2) { constrainedBitSamples = 2 }
+        if (constrainedBitSamples > 15) { constrainedBitSamples = 15 }
+    
+        var constrainedNormFrequency = _normFrequency;
+        if (constrainedNormFrequency < 0.1) { constrainedNormFrequency = 0.1 }
+        if (constrainedNormFrequency > 1) { constrainedNormFrequency = 1 }
+
+        effectBitcrushContainer.current = createBitCrush(constrainedBitSamples, constrainedNormFrequency)();
+
+        // Change distortion curve, 0 - 100
+        var constrainedDistortion = _distortion;
+        if (constrainedDistortion < 0) { constrainedDistortion = 0 }
+        if (constrainedDistortion > 100) { constrainedDistortion = 100 }
+        effectDistortionContainer.current.curve = makeDistortionCurve(constrainedDistortion);
+
+        // Change Bass Boost
+        effectBassBoostContainer.current.gain.value = _bassBoost;
+
+        // Change Gain
+        // Logarithmic scale for easier usabilit<; the middle is loudness = 1 (unchanged)
+        var logGain = 0.008 * Math.pow(_gain, 3);
+        effectGainContainer.current.gain.value = logGain;
 
         function makeDistortionCurve(amount) {
-            var k = typeof amount === 'number' ? amount : 0,
+            var k = amount,
                 n_samples = 44100,
                 curve = new Float32Array(n_samples),
                 deg = Math.PI / 180,
@@ -109,29 +153,25 @@ function AudioManipulator({ videoRef }) {
             }
             return curve;
         };
-        return () => {
-            audioSrcContainer.current.disconnect();
-            gain.disconnect();
-            dist.disconnect()
-            bitcrush.disconnect();
-            filter.disconnect();
-        }
-    }, [_bassBoost, _bitSamples, _bitcrushNormFrequency, _distortion, _gain, videoRef, audioCtxContainer]);
-
-//TODO: implement user control for audio filters
+    }, [_bassBoost, _bitSamples, _normFrequency, _distortion, _gain]);
 
     return (
         <div>
             <label>Bitcrush Bit-Reduction</label>
-            <input type="range" min="0" max="16" step="1" value={_bitSamples} onChange={e => {setBitSamples(e.target.value)}}/>
-            <label>Bitcrush Norm.-Frequency</label>
-            <input type="range" min="0" max="1" step="0.01" value={_bitcrushNormFrequency} onChange={e => {setBitcrushNormFrequency(e.target.value)}}/>
+            <input type="range" min="1" max="16" step="1" value={_bitSamples} onChange={e => {setBitSamples(e.target.value)}}/>
+
+            <label>Bitcrush Norm-Frequency</label>
+            <input type="range" min="0" max="1" step="1" value={_normFrequency} onChange={e => {setNormFrequency(e.target.value)}}/>
+
             <label>Distortion</label>
-            <input type="range" min="0" max="100" step="0.1" value={_distortion} onChange={e => {setDistortion(e.target.value)}}/>
+            <input type="range" min="-0.25" max="30.25" step="0.25" value={_distortion} onChange={e => {setDistortion(e.target.value)}}/>
+
             <label>Bass Boost</label>
             <input type="range" min="0" max="100" step="1" value={_bassBoost} onChange={e => {setBassBoost(e.target.value)}}/>
+
             <label>Gain</label>
             <input type="range" min="0" max="10" step="0.01" value={_gain} onChange={e => {setGain(e.target.value)}}/>
+
             <label>Reset Sound FX</label>
             <input type="button" value="Reset" onClick={(handleResetClick)}></input>
         </div>
