5bd8fb2e02f88779b54d2a78e1a0ae8300ad3c56
Zombatar33 <ageeadebied@gmail.com>
2023-01-25 15:27:24 +0100

    Fixed AudioFX Controls

--- src/components/AudioManipulation.jsx
+++ src/components/AudioManipulation.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState, useRef } from 'react';
 
 function AudioManipulator({ videoRef }) {
+
+    const audioCtxContainer = useRef(null);
+    const audioSrcContainer = useRef(null);
+    const audioScriptContainer = useRef(null)
+
     var [_bitSamples, setBitSamples] = useState(7);
     const [_bitcrushNormFrequency, setBitcrushNormFrequency] = useState(0.2);
     const [_distortion, setDistortion] = useState(0);
     const [_bassBoost, setBassBoost] = useState(0);
     const [_gain, setGain] = useState(1);
-    const audioContextRef = useRef(null);
-    const mediaElementSourceRef = useRef(null);
 
     function handleResetClick() {
-        setBitSamples(7);
+        setBitSamples(0);
         setBitcrushNormFrequency(1);
         setDistortion(0);
         setBassBoost(0);
@@ -18,35 +21,39 @@ function AudioManipulator({ videoRef }) {
     }
 
     function handlePlay() {
-        audioContextRef.current.resume();
+        if (audioCtxContainer.current.state === "running") {
+            audioCtxContainer.current.suspend();
+        }else {
+            audioCtxContainer.current.resume();
+        }
     }
-
-    useEffect(() => {
-        audioContextRef.current = new AudioContext();
-        const video = videoRef.current;
-        mediaElementSourceRef.current = audioContextRef.current.createMediaElementSource(video);
-        return () => {
-            audioContextRef.current.close()
-        };
-    }, []);
 
     useEffect(() => {
         const video = videoRef.current;
         video.onplay = handlePlay;
         return () => {
-            video.onplay = null;
+          video.onplay = null;
         }
-    }, []);
+      }, []);
 
     useEffect(() => {
-        if (!audioContextRef.current) return;
-        //const audioSource = mediaElementSourceRef.current;
-        //const context = audioContextRef.current;
+        if (!audioCtxContainer.current) {
+            audioCtxContainer.current = new AudioContext();
+        }
+        if (!audioSrcContainer.current) {
+            audioSrcContainer.current = audioCtxContainer.current.createMediaElementSource(videoRef.current);
+        }
+
+        if (!audioScriptContainer.current) {
+            var bufferSize = 4096;
+            audioScriptContainer.current = audioCtxContainer.current.createScriptProcessor(bufferSize, 1, 1);
+        }
+
 
         //bitcrush
-        var bufferSize = 4096;
         var bitcrush = (function () {
-            var node = audioContextRef.current.createScriptProcessor(bufferSize, 1, 1);
+            var bufferSize = 4096;
+            var node = audioScriptContainer.current;
             node.bits = _bitSamples; // between 1 and 16
             node.normfreq = _bitcrushNormFrequency; // between 0.0 and 1.0
             var step = Math.pow(1 / 2, node.bits);
@@ -68,26 +75,26 @@ function AudioManipulator({ videoRef }) {
         })();
 
         // create distortion
-        var dist = audioContextRef.current.createWaveShaper();
+        var dist = audioCtxContainer.current.createWaveShaper();
         // 0 bis 100 funktioniert sehr gut, ist aber egal
         dist.curve = makeDistortionCurve(_distortion);
 
         // create filter
-        var filter = audioContextRef.current.createBiquadFilter();
+        var filter = audioCtxContainer.current.createBiquadFilter();
         filter.type = "lowshelf";
         filter.frequency.value = 200;
         filter.gain.value = _bassBoost;
 
         // gain
-        var gain = audioContextRef.current.createGain();
+        var gain = audioCtxContainer.current.createGain();
         gain.gain.value = _gain;
 
         // chain effects - first effect in chain is actually last
-        mediaElementSourceRef.current.connect(gain);
+        audioSrcContainer.current.connect(gain);
         gain.connect(dist);
         dist.connect(bitcrush);
         bitcrush.connect(filter);
-        filter.connect(audioContextRef.current.destination);
+        filter.connect(audioCtxContainer.current.destination);
 
         function makeDistortionCurve(amount) {
             var k = typeof amount === 'number' ? amount : 0,
@@ -103,28 +110,28 @@ function AudioManipulator({ videoRef }) {
             return curve;
         };
         return () => {
-            mediaElementSourceRef.current.disconnect();
+            audioSrcContainer.current.disconnect();
             gain.disconnect();
             dist.disconnect()
             bitcrush.disconnect();
             filter.disconnect();
         }
-    }, [_bassBoost, _bitSamples, _bitcrushNormFrequency, _distortion, _gain, videoRef]);
+    }, [_bassBoost, _bitSamples, _bitcrushNormFrequency, _distortion, _gain, videoRef, audioCtxContainer]);
 
-    //TODO: implement user control for audio filters
+//TODO: implement user control for audio filters
 
     return (
         <div>
             <label>Bitcrush Bit-Reduction</label>
-            <input type="range" min="0" max="16" step="1" value={_bitSamples} onChange={e => { setBitSamples(e.target.value) }} />
+            <input type="range" min="0" max="16" step="1" value={_bitSamples} onChange={e => {setBitSamples(e.target.value)}}/>
             <label>Bitcrush Norm.-Frequency</label>
-            <input type="range" min="0" max="1" step="0.01" value={_bitcrushNormFrequency} onChange={e => { setBitcrushNormFrequency(e.target.value) }}/>
+            <input type="range" min="0" max="1" step="0.01"/>
             <label>Distortion</label>
-            <input type="range" min="0" max="100" step="1" value={_distortion} onChange={e => { setDistortion(e.target.value) }} />
+            <input type="range" min="0" max="100" step="0.1"/>
             <label>Bass Boost</label>
-            <input type="range" min="0" max="100" step="1" value={_bassBoost} onChange={e => { setBassBoost(e.target.value) }} />
+            <input type="range" min="0" max="100" step="1"/>
             <label>Gain</label>
-            <input type="range" min="0" max="10" step="0.01" value={_gain} onChange={e => { setGain(e.target.value) }}/>
+            <input type="range" min="0" max="10" step="0.01"/>
             <label>Reset Sound FX</label>
             <input type="button" value="Reset" onClick={(handleResetClick)}></input>
         </div>
